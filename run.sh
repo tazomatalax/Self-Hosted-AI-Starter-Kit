b7f2603c404bd46b11b5f26d5e401ac267ecee9b
tazomatalax <tazomatalax@hotmail.com>
2024-10-28 17:08:08 +1300

    update scripts

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker compose --profile gpu-nvidia up -d
 
 # Display the host IP and Homer URL
 echo "Docker containers have been started."
-echo "Access your services at: http://localhost:8080 (Homer URL)"
+echo "Access your services at: http://localhost:3333 (Homepage URL)"
--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ docker compose create && docker compose --profile gpu-nvidia up -d
 
 # Display the host IP and Homer URL
 echo "Docker containers have been updated."
-echo "Access your services at: http://localhost:3333 (Homer URL)"
+echo "Access your services at: http://localhost:3333 (Homepage URL)"
