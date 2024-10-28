7cede4b0a8284a00dea8f2a4e1e608cf485e1eb4
tazomatalax <tazomatalax@hotmail.com>
2024-10-28 15:40:19 +1300

    update homer config

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Automatically export the host IP
-export HOST_IP=$(hostname -I | awk '{print $1}')
 
-# Create a .env file with the HOST_IP
-echo "HOST_IP=$HOST_IP" > .env
 
 # Run docker-compose
 docker compose --profile gpu-nvidia up -d
 
 # Display the host IP and Homer URL
 echo "Docker containers have been started."
-echo "Access your services at: http://$HOST_IP:8080 (Homer URL)"
+echo "Access your services at: http://localhost:8080 (Homer URL)"
--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 docker compose --profile gpu-nvidia pull
 docker compose create && docker compose --profile gpu-nvidia up -d
